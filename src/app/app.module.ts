f1b2aa75169da84ec8992e3223093462ebb34ae4
Gaston Corvalan <corvata@gmail.com>
2022-08-15 18:38:45 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AvatarComponent,
+    BannerComponent,
+    NavbarComponent,
+    LoginComponent,
+    EducationComponent,
+    ExperienceComponent,
+    LogoComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    AboutComponent,
+    FooterComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
